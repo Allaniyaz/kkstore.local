048d5283df9ddc146a049b55cd6c2fd8128d2080
allaniyaz <a.nuraximov@uzinfocom.uz>
2022-11-25 18:15:55 +0500

    added filament resource and softdeletes for roles

--- database/migrations/2022_11_22_050105_create_roles_table.php
+++ database/migrations/2022_11_22_050105_create_roles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
-            $table->string('description', 255);
+            $table->string('description', 255)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
